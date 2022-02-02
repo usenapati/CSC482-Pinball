ba3a6dfca91ec4db0fad6880b65aa6dd3f9e018c
Udayan Senapati <udayansenapati@gmail.com>
2022-02-02 16:16:45 -0500

    Minor Additions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,15 +10,24 @@ public class GameManager : MonoBehaviour
     public string BestScoreName = "BestScore";
 
 
-    private bool paused = false;
+    private bool b_paused = false;
 
     // Gameplay Variables
+    [Header("Game Objects")]
+    // Ball
+    // Plunger
+    // Drain
+    // Flippers
+    // Bumpers
+
+
     [Header("Player Life and Score")]
     public int Lives = 3;                       // Max Lives
     private int currentLives = 0;               // Current Lives
+    private int roundScore = 0;                 // Score during a round
     private int playerScore = 0;                // Player Score
     private int numBall;                        // The number of ballss played by the player
-    private bool startGame;                     // True : Player start the game . False : Game is over
+    private bool b_startGame;                     // True : Player start the game . False : Game is over
 
     [Header("Tilt Mode")]
     public float minTimeTilt = 1;	            // Minimum time in seconds between two shake
@@ -27,41 +36,77 @@ public class GameManager : MonoBehaviour
 
 
     [Header("Mode Multi Ball")]
-    public GameObject objMultiBall;             // Object that manage the multi-ball on playfield. Manage how the ball is ejected on playfield
+    public GameObject obj_MultiBall;             // Object that manage the multi-ball on playfield. Manage how the ball is ejected on playfield
     public MultiBall multiBall;                 // Access MultiBall component from obj_Launcher_MultiBall gameObject;
     private int ballsOnBoard = 0;               // Know the number of board. 
-    private bool mutiBallState = false;         // Mode Multi ball activate or not
+    private bool b_mutiBallState = false;         // Mode Multi ball activate or not
     private int maxBallsSpawn = 3;              // Number of Balls in muti ball mode
 
     [Header("Bonus Extra Ball")]
     public bool extraBall = false;
 
     [Header("Bonus Ball Saver")]
-    public bool startGameWithBallSaver = false; // If true : player start a new ball with BallSaver
+    public bool b_startGameWithBallSaver = false; // If true : player start a new ball with BallSaver
     public int startDuration = -1;
-    public bool ballSaver = false;              // True : Ball Saver is enabled
+    public bool b_ballSaver = false;              // True : Ball Saver is enabled
 
     [Header("Bonus Multiplier")]                // Bonus Multiplier = multiplier x hitCounter + mulitplierSuperBonus)
     public int multiplier = 1;                  // multiplier could be x1 x2 x4 x6 x 8 x10							
     public int mulitplierSuperBonus = 1000000;  // Add points if multiplier = 10
     public int hitCounter = 0;                  // Record the number of object that hit the ball during the current ball
 
-
+    [Header("Ball Lost")]
+    // Ball Out
+    public float timeBallout = 2;               // Duration of ball lost
+    private float tmpBalloutTime = 0;
+    // Bonus Calculation 
+    // Next Ball or Game Over
 
     // UI
+    [Header("LCD Text")]
+    private Text GUI_Txt_Timer;
+    private Text GUI_Txt_Info_Ball;
+    private Text GUI_Txt_Score;
+
+    // Use to display text on LCD screen
+    public string[] arr_Info_Txt;
+    private float tmp_Time;
+    private float TimeBetweenTwoInfo;
+    private bool b_Txt_Info = true;
+
+    [Header("Text used during game")]
+    public string[] Txt_Game;                   // Array : All the text use by the game Manager
+
+
+    [Header("Plunger")]
+    private GameObject spawnBall;               // (connected automatically) The GameObject that manage the ejection after a ball respawn
+
+
+    [Header("Ball")]
+    public Transform ball;                      // Connect the ball Prefab
+
+
+    // SFX
+    public AudioClip s_Load_Ball;					// play a sound when the ball respawn
+    public AudioClip a_LoseBall;                // Play a sound when the player lose a ball
+    public AudioClip a_Bonus_Screen;			// Play a sound during the bonus score 
+    public AudioClip a_GameOver;			    // Play a sound during the bonus score 
+    
+
+    
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // START STATE
