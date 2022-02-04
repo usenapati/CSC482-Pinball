d7556fd1b464aa12dcd163a6d58b8f6fd5c0ce1e
Will <willcarpenter3@yahoo.com>
2022-02-04 02:04:42 -0500

    refined level, added new prefabs

---
+++ Assets/Scripts/DropBumper.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBumper : MonoBehaviour
+{
+
+    public float forceAmount = 35f;
+
+    public float dropSpeed = 10f;
+
+    public bool respawn = false;
+
+    private bool hit = false;
+    private Vector3 targetPos;
+    private Vector3 originalPos;
+
+    void Start()
+    {
+        originalPos = transform.position;
+        targetPos = new Vector3(transform.position.x, transform.position.y - 10, transform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        if (hit)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, dropSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, originalPos, dropSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Colliding");
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("Explosion Force");
+            other.rigidbody.AddExplosionForce(forceAmount, transform.position, 3f, 0, ForceMode.Impulse);
+            hit = true;
+
+            if (respawn)
+            {
+                StartCoroutine("Respawn");
+            }
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(30);
+        hit = false;
+    }
+}
